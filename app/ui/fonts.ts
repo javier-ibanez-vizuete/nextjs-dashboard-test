4101b6637441f38afbcb7d32341aff282f911c9e
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2026-02-12 16:22:35 +0100

    fix fonts names

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,5 @@
 import { Inter, Lusitana } from "next/font/google"
 
-export const interFont = Inter({ subsets: ["latin"] })
+export const inter = Inter({ subsets: ["latin"] })
 
-export const lusitanaFont = Lusitana({ weight: ["400", "700"], subsets: ["latin"] })
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] })
